8c5bcf7eed9e04e972749413edfa3985bd07d497
Ozan Unsal <ounsal@redhat.com>
2021-12-14 11:54:54 +0100

    Refactor

--- calculator-microservices/multiplier/src/test/java/com/redhat/training/MultiplierResourceTest.java
+++ calculator-microservices/multiplier/src/test/java/com/redhat/training/MultiplierResourceTest.java
@@ -16,4 +16,18 @@ public class MultiplierResourceTest {
         solverService = mock(SolverService.class);
         multiplierResource = new MultiplierResource(solverService);
     }
+
+
+    @Test
+    public void simpleMultiplication() {
+        // Given
+        Mockito.when(solverService.solve("2")).thenReturn(Float.valueOf("2"));
+        Mockito.when(solverService.solve("3")).thenReturn(Float.valueOf("3"));
+
+        // When
+        Float result = multiplierResource.multiply("2", "3");
+
+        // Then
+        assertEquals( 6.0f, result );
+    }
 }
--- calculator-monolith/src/main/java/com/redhat/training/SolverResource.java
+++ calculator-monolith/src/main/java/com/redhat/training/SolverResource.java
@@ -12,6 +12,7 @@ import com.redhat.training.operation.Add;
 import com.redhat.training.operation.Identity;
 import com.redhat.training.operation.Operation;
 import com.redhat.training.operation.Substract;
+import com.redhat.training.operation.Multiply;
 import com.redhat.training.service.SolverService;
 
 import org.slf4j.Logger;
@@ -27,13 +28,16 @@ public final class SolverResource implements SolverService {
     Substract substract;
 
     @Inject
+    Multiply multiply;
+
+    @Inject
     Identity identity;
 
     private List<Operation> operations;
 
     @PostConstruct
     public void buildOperationList() {
-        operations = List.of(substract, add, identity);
+        operations = List.of(substract, add, multiply, identity);
     }
 
     @Override
